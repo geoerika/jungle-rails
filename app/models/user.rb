311085b7e60abb1053c579b7b5a3a037f0157f73
Erika Szabo <erika.szabo.geo@gmail.com>
2018-11-12 20:13:31 +0000

    required password to have min 3 characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false}
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 3,
+    too_short: "is too short (minimum is %{count} characters)" }
   validates :password_confirmation, presence: true
 
 end
