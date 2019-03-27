c34d791b7044da9989f553f6a54b5b7372e599f7
Erika Szabo <erika.szabo.geo@gmail.com>
2019-03-27 11:51:15 +0000

    just minor editing

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,17 +1,10 @@
 class Admin::ProductsController < ApplicationController
 
-
    http_basic_authenticate_with name: ENV["ADMIN_NAME"], password: ENV["ADMIN_PASSWORD"]
-   # , except: :index
-
-   # def index
-   #   render plain: "Everyone can see me!"
-   # end
 
    def edit
      render plain: "I'm only accessible if you know the password"
    end
-
 
   def index
     @products = Product.order(id: :desc).all
